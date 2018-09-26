f5a021943cd85fdef9eb91a0904ca47489afd253
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-26 22:15:09 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180926221440) do
+
+  create_table "users", force: :cascade do |t|
+    t.string  "name"
+    t.integer "uid"
+  end
 
 end
