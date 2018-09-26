7037fee0a732c6b857d013633a85800e41a6b69d
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-26 22:03:02 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180926220233) do
+
+  create_table "users", force: :cascade do |t|
+  end
 
 end
